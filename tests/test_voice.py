69c53aa33eebfd5285bc2961e7af002227d6796e
santialexluna-blip <santialexluna@gmail.com>
2026-09-13 15:47:07 -0600

    test: add v2.1 wake word coverage

---
+++ tests/test_voice.py
@@ -0,0 +1,13 @@
+from voice import VoiceEngine
+
+
+def test_wake_word_is_required():
+    assert VoiceEngine.strip_wake_word("hola") is None
+
+
+def test_wake_word_alone_activates():
+    assert VoiceEngine.strip_wake_word("JARVIS") == ""
+
+
+def test_wake_word_extracts_command():
+    assert VoiceEngine.strip_wake_word("JARVIS busca noticias") == "busca noticias"
